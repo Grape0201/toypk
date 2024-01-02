655346f8085fe721edc07205224f36f3559de0dd
Grape0201 <kusa.kusa.rpi@gmail.com>
2024-01-02 11:51:43 +0900

    now, get_linear_attenuation_coefficient recieves and returns array

--- python/toypk/lac/__init__.py
+++ python/toypk/lac/__init__.py
@@ -1,25 +1,26 @@
 from .db import db
 from scipy.interpolate import Akima1DInterpolator
+import numpy as np
 
 
 def get_linear_attenuation_coefficient(
-    energy: float, densities: dict[str, float]
-) -> float:
+    energy_points: list[float], densities: dict[str, float]
+) -> list[float]:
     """
     Parameters
     ----------
-    energy : float
+    energy_points : list[float]
         photon ray energy [MeV]
     densities: dit[str, float]
-        the key is the element like H, the value is the density [g/cm3]
+        the key is the element like "H", the value is the density [g/cm3]
 
     Returns
     -------
-    lac: float
-        linear attenuation coefficient
+    lac: list[float]
+        Akima-interpolated linear attenuation coefficients
     """
-    lac = 0.0
+    lac = np.zeros(len(energy_points))
     for element, rho in densities.items():
-        mu_rho = Akima1DInterpolator(db[element]["e"], db[element]["mu_rho"])(energy)
-        lac += mu_rho * rho
-    return lac
+        a = Akima1DInterpolator(db[element]["e"], db[element]["mu_rho"])
+        lac += np.array(list(map(lambda e: rho * a(e), energy_points)))
+    return lac.tolist()
